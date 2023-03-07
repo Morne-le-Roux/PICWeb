c0122dd75f996ac466acbd097a105eb086fc6e88
Morné le Roux <mornelerx@gmail.com>
2023-03-07 20:54:59 +0200

    implenented basic carousel

--- lib/carousel_banner.dart
+++ lib/carousel_banner.dart
@@ -14,14 +14,13 @@ class _BannerCarouselState extends State<BannerCarousel> {
   Widget build(BuildContext context) {
     List<Widget> advertisements =
         []; //List of the advertisements for the top banner. Currently only supports 1-4. Will be changed after CMS is developed.
-    advertisements.add(AspectRatio(
-      aspectRatio: 16 / 4.5, //aspect ratio keeps the image the right size
-      child: Container(
-        decoration: const BoxDecoration(
-            image: DecorationImage(image: AssetImage("Ad_1.jpg"))),
-      ),
+    advertisements.add(Container(
+      decoration: const BoxDecoration(
+          image: DecorationImage(image: AssetImage("Ad_1.jpg"))),
     ));
 
-    return const Placeholder();
+    return CarouselSlider(
+        options: CarouselOptions(height: 550, viewportFraction: 1),
+        items: advertisements);
   }
 }
